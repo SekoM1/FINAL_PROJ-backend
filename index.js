76e1f1029fe7e38635ae052ba3085f14a1eae835
AshtonMar <ashtonmartingoliath@gmail.com>
2022-03-19 15:05:24 +0200

    make it better

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-// require ("dotenv").config();   
+// require ("dotenv").config();
 
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const menuRoute = require('./Routes/menu');
+const menuRoute = require("./Routes/menu");
 const userRoute = require("./Routes/user");
-const contactRoute = require("./Routes/contact")
-const authRoute = require("./middleware/auth")
-const bcrypt = require("bcrypt")
-
+const contactRoute = require("./Routes/contact");
+const authRoute = require("./middleware/auth");
+const bcrypt = require("bcrypt");
 
 // app.post()
 
@@ -22,12 +21,13 @@ mongoose
 
 app.use(express.json());
 app.use("/users", userRoute);
-app.use("/contact", contactRoute)
+app.use("/contact", contactRoute);
 
 app.use("/menu", menuRoute);
 
-
-
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Backend server is running");
