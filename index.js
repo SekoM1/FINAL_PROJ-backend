2408b29b81c1a7d478807300da2d9b4182c6c4ef
AshtonMar <ashtonmartingoliath@gmail.com>
2022-03-19 14:22:35 +0200

    add new changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// require ("dotenv").config();   
+require ("dotenv").config();   
 
 const express = require("express");
 const app = express();
@@ -29,6 +29,6 @@ app.use("/menu", menuRoute);
 
 
 
-app.listen(process.env.PORT || 1300, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Backend server is running");
 });
