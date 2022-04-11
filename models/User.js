16895df7554f7f060cf95963ef837a2c2757fd30
nomvuyiseko <nomvuyisekompofu@gail.com>
2022-04-11 13:01:58 +0200

    updated user profile

--- models/User.js
+++ models/User.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose")
 const UserSchema = new mongoose.Schema(
     {
-        firstname: {type: String, required: true, unique:true},
-        lastname:{type: String, required: true, unique: true},
-        email: {type: String, required:true},
+        firstname: {type: String, required: true},
+        lastname:{type: String, required: true},
+        email: {type: String, required:true, unique: true},
         password: { type: String, required: true},
         isAdmin:{
             type: Boolean,
